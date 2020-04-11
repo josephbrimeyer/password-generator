cf50387201bde866a502e5b3789b2e331dab6b9e
Joe Brimeyer <jbrimeyer@gmail.com>
2020-04-10 21:01:12 -0700

    added arrays.

--- Develop/script.js
+++ Develop/script.js
@@ -7,16 +7,53 @@ let upCase = confirm("Do you want upper case letters in your password?");
 let lowCase = confirm("Would you like to have lower case letters in your password?");
 let nums = confirm("Would you like to have numbers in your password?");
 let syms = confirm("would you like to have special characters in your password?");
-
+let input;
+//if (passwordLength < 8 || passwordLength > 128) {
+//alert("Please select a number between 8 and 128.")
+//}
 
 console.log(passwordLength);
 console.log(upCase);
 console.log(nums);
 console.log(syms);
 
+let upCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"];
+let lowCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let numsArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let symsArray = ["!", "#", ",", "$", "%", "&", "'", "(", ")", "*", "+", "-",
+  ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+
+
+
+while (passwordLength > 8 || passwordLength < 128) {
+  passwordLength = prompt("How many characters would you like to have in your password?");
+
+  // If the letter is h, run the following functions/methods.
+  if (letter === "h") {
+    car.honk();
+    reWriteStats();
+  }
+  // If the letter is d, run the following functions/methods.
+  else if (letter === "d" && car.isWorking) {
+    car.driveToWork();
+    reWriteStats();
+  }
+  // If the letter is w, run the following functions/methods.
+  else if (letter === "w" && car.isWorking) {
+    car.driveAroundWorld();
+    reWriteStats();
+  }
+  // If the letter is t, run the following functions/methods.
+  else if (letter === "t") {
+    car.getTuneUp();
+    reWriteStats();
+  }
+}
+
+
 // for loop for random number
 for (let j = 0; j < passwordLength; j++) {
-  let nums = Math.floor(Math.random()*10);
+  let nums = Math.floor(Math.random() * 10);
   console.log(nums);
 }
 
