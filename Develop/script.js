9466fe0c114691bc4d46d0b3e9e79464a4baa678
Joe Brimeyer <jbrimeyer@gmail.com>
2020-04-10 19:02:09 -0700

    set initial js variables.

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,47 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
+let passwordLength = prompt("How many characters would you like to have in your password?");
+let minLength = 8;
+let maxLength = 128;
+let upCase = confirm("Do you want upper case letters in your password?");
+let lowCase = confirm("Would you like to have lower case letters in your password?");
+let nums = confirm("Would you like to have numbers in your password?");
+let syms = confirm("would you like to have special characters in your password?");
+
+
+console.log(passwordLength);
+console.log(upCase);
+console.log(nums);
+console.log(syms);
+
+//let newString = "";
+
+
+//if (nums) {
+  //newString.
+//}
+
+//let userInput = {
+  //passwordLength: (answer),
+  //upperCase: (answer),
+  //lowerCase: (answer),
+  //numbers: (answer),
+  //symbols: (answer),
+
+//}
+//console.log(userInput);
+
+//var car = {
+  //make: "Honda",
+  //model: "Fit",
+  //color: "Blue Raspberry",
+  //mileage: 3000,
+  //isWorking: true,
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
