2a711cebeac76eefdcfa964bded1bf4c3df5312a
Joe Brimeyer <jbrimeyer@gmail.com>
2020-04-14 19:43:54 -0700

    added for loop functionality.

--- Develop/script.js
+++ Develop/script.js
@@ -6,7 +6,7 @@ let lowerString = "abcdefghijklmnopqrstuvwxyz"
 let numberString = "0123456789";
 let symbolString = "!#$%&()*+-^<[?@]";
 let myString = "";
-let answer;
+let finalPassword = "";
 
 function writePassword() {
   let password = generatePassword();
@@ -15,25 +15,6 @@ function writePassword() {
   passwordText.value = password;
   console.log();
 }
-// function useableCharacters(upperC, lowerC, numbers, symbols) {
-//   if (upperC) {
-//     myString += upperString
-//     console.log(myString)
-//   }
-//   if (lowerC) {
-//     myString += lowerString
-//     console.log(myString)
-//   }
-//   if (numbers) {
-//     myString += numberString
-//     console.log(myString)
-//   }
-//   if (symbols) {
-//     myString += symbolString
-//     console.log(myString)
-//   }
-//   }
-
 function userInput() {
   passwordLength = prompt("How many characters would you like your password to be?");
   while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
@@ -42,33 +23,29 @@ function userInput() {
   let upperC = confirm("Do you want upper case letters in your password?");
   if (upperC) {
     myString += upperString
-    console.log(myString)
-  }
+    }
   let lowerC = confirm("Would you like to have lower case letters in your password?");
   if (lowerC) {
     myString += lowerString
-    console.log(myString)
-  }
+    }
   let numbers = confirm("Would you like to have numbers in your password?");
   if (numbers) {
     myString += numberString
-    console.log(myString)
-  }
+    }
   let symbols = confirm("Would you like to have special characters in your password?");
   if (symbols) {
     myString += symbolString
-    console.log(myString)
-  }
+    }
   if (upperC === false && lowerC === false && numbers === false && symbols === false) {
     alert("You must select at least one of the criteria!  Please try again.");
     return;
   }
-  
-  //useableCharacters()
+  console.log(myString);
+  console.log(passwordLength);
+
   generatePassword()
   writePassword()
 }
-let finalPassword = "";
 function generatePassword() {
   
   for (let i = 0; i < myString.length; i++) {
@@ -79,14 +56,4 @@ function generatePassword() {
   }
   return finalPassword;
 }
-// Add event listener to generate button
 generateBtn.addEventListener("click", userInput);
-//............................................................................................................
-// Write password to the #password input
-//function writePassword() {
- // var password = generatePassword();
-  //var passwordText = document.querySelector("#password");
-  //passwordText.value = password;
-//}
-// Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword);
